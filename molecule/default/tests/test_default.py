a74bc3799ba91dd864511d75bcf27bccb6a28fab
Josh Benner <josh@bennerweb.com>
2018-07-22 19:56:26 -0400

    Trust certificates; configure LDAP

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -9,10 +9,17 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 def test_rundeck_package(host):
     package = host.package('rundeck')
     assert package.is_installed
-    assert package.version == '2.11.5
+    assert package.version == '2.11.5'
 
 
 def test_rundeck_service(host):
     service = host.service('rundeckd')
     assert service.is_running
     assert service.is_enabled
+
+def test_trusted_cert(host):
+    out = host.check_output('keytool -list '
+                            '-keystore /etc/rundeck/ssl/truststore '
+                            '-storepass adminadmin')
+    assert 'snakeoil' in out
+    assert 'rundeck' in out
